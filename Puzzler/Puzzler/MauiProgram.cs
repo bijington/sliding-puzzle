9ccd8ddf41bc94a7a399ea57a751598946354d9b
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-07-19 21:52:43 +0100

    Embedded the IonIcons font

--- Puzzler/Puzzler/MauiProgram.cs
+++ Puzzler/Puzzler/MauiProgram.cs
@@ -13,7 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("ionicons.ttf", "IonIcons");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
