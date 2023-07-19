41cb028b118cfd83614d0a801feb8066e1ca322d
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-07-19 21:08:47 +0100

    Created our data models

---
+++ Puzzler/Puzzler/Level.cs
@@ -0,0 +1,10 @@
+﻿namespace Puzzler;
+
+public class Level
+{
+	public string Name { get; init; }
+
+	public string ImageName { get; init; }
+
+	public LevelDifficulty Difficulty { get; init; }
+}
---
+++ Puzzler/Puzzler/LevelDifficulty.cs
@@ -0,0 +1,19 @@
+﻿namespace Puzzler;
+
+public enum LevelDifficulty
+{
+    /// <summary>
+    /// Easy difficulty - grid size of 2x2
+    /// </summary>
+    Easy = 0,
+
+    /// <summary>
+    /// Medium difficulty - grid size of 3x3
+    /// </summary>
+    Medium = 1,
+
+    /// <summary>
+    /// Hard difficulty - grid size of 4x4
+    /// </summary>
+    Hard = 2
+}
