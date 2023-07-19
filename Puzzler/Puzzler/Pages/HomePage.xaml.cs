cb4a556a1dace1975a522f1b8495aeb805071b3e
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-07-19 21:52:55 +0100

    Created our home page

---
+++ Puzzler/Puzzler/Pages/HomePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Puzzler.Pages;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    void OnPlayButtonClicked(object sender, EventArgs e)
+    {
+    }
+}
