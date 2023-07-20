26277e03664b12b0cbbd867c3476979941d6dbdb
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-07-20 08:41:05 +0100

    Created our level selection page

---
+++ Puzzler/Puzzler/Pages/LevelSelectionPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Puzzler.Pages;
+
+public partial class LevelSelectionPage : ContentPage
+{
+    public LevelSelectionPage(Context context)
+    {
+        InitializeComponent();
+
+        LevelsCollectionView.ItemsSource = context.Levels;
+    }
+}
