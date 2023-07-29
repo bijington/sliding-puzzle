344b2ee0594b25318237d1af33b7a941862361a0
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-07-29 22:03:32 +0100

    Navigated to our new page

--- Puzzler/Puzzler/Pages/LevelSelectionPage.xaml.cs
+++ Puzzler/Puzzler/Pages/LevelSelectionPage.xaml.cs
@@ -8,4 +8,22 @@ public partial class LevelSelectionPage : ContentPage
 
         LevelsCollectionView.ItemsSource = context.Levels;
     }
+
+    private async void OnLevelsCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var level = e.CurrentSelection.FirstOrDefault();
+
+        if (level is null)
+        {
+            return;
+        }
+
+        await Shell.Current.GoToAsync(nameof(LevelPage), new Dictionary<string, object>
+        {
+            [nameof(LevelPage.Level)] = level
+        });
+
+        // Clear the selection for when we navigate back nothing is selected.
+        LevelsCollectionView.SelectedItem = null;
+    }
 }
